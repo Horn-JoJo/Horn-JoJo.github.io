0ded6bd95e790a94e834c7d3a62bd4615e6b8ec7
JOJO <1901381900@qq.com>
2023-11-07 23:10:47 +0800

    Site updated: 2023-11-07 23:10:18

--- js/toggleImage.js
+++ js/toggleImage.js
@@ -1,17 +1,17 @@
 const images = [
     "https://picbed.kuroko.love/blog_background/%E9%A3%9F%E8%9C%82%E6%93%8D%E7%A5%88.jpg",
-    "https://picbed.kuroko.love/blog_background/10032.png",
-    "https://picbed.kuroko.love/blog_background/%E4%B8%BB%E8%A7%92%E5%9B%A2.jpg",
-    "https://picbed.kuroko.love/blog_background/%E4%B8%BB%E8%A7%92%E5%9B%A22.jpg",
+    // "https://picbed.kuroko.love/blog_background/10032.png",
+    // "https://picbed.kuroko.love/blog_background/%E4%B8%BB%E8%A7%92%E5%9B%A2.jpg",
+    // "https://picbed.kuroko.love/blog_background/%E4%B8%BB%E8%A7%92%E5%9B%A22.jpg",
     "https://picbed.kuroko.love/blog_background/%E4%B8%BB%E8%A7%92%E5%9B%A23.jpg",
-    "https://picbed.kuroko.love/blog_background/%E5%91%B1%E5%A4%AA%E7%89%88%E7%BE%8E%E7%90%B4.png",
+    // "https://picbed.kuroko.love/blog_background/%E5%91%B1%E5%A4%AA%E7%89%88%E7%BE%8E%E7%90%B4.png",
     "https://picbed.kuroko.love/blog_background/%E5%A9%9A%E7%BA%B1%E6%B3%AA%E7%88%B7.jpg",
-    "https://picbed.kuroko.love/blog_background/%E5%B0%8F%E5%BE%A1%E5%9D%82.jpg",
+    // "https://picbed.kuroko.love/blog_background/%E5%B0%8F%E5%BE%A1%E5%9D%82.jpg",
     "https://picbed.kuroko.love/blog_background/%E5%BE%A1%E5%9D%82%E7%BE%8E%E7%90%B4and%E4%B8%8A%E6%9D%A1%E5%BD%93%E9%BA%BB.webp",
-    "https://picbed.kuroko.love/blog_background/%E5%BE%A1%E5%9D%82%E7%BE%8E%E7%90%B4and%E7%99%BD%E4%BA%95%E9%BB%91%E5%AD%90.png",
-    "https://picbed.kuroko.love/blog_background/%E6%B3%AA%E7%88%B7and%E5%88%9D%E6%98%A5.jpg",
-    "https://picbed.kuroko.love/blog_background/%E7%99%BD%E4%BA%95%E9%BB%91%E5%AD%90.png",
-    "https://picbed.kuroko.love/blog_background/%E9%A3%9F%E8%9C%82%E6%93%8D%E7%A5%882.jpg",
+    // "https://picbed.kuroko.love/blog_background/%E5%BE%A1%E5%9D%82%E7%BE%8E%E7%90%B4and%E7%99%BD%E4%BA%95%E9%BB%91%E5%AD%90.png",
+    // "https://picbed.kuroko.love/blog_background/%E6%B3%AA%E7%88%B7and%E5%88%9D%E6%98%A5.jpg",
+    // "https://picbed.kuroko.love/blog_background/%E7%99%BD%E4%BA%95%E9%BB%91%E5%AD%90.png",
+    // "https://picbed.kuroko.love/blog_background/%E9%A3%9F%E8%9C%82%E6%93%8D%E7%A5%882.jpg",
 ];
 
 /**
